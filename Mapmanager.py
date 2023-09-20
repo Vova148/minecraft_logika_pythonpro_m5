d7fc1d67dc2639d480efc80bd66b3d9f9d966afa
Volodya132 <56843184+Volodya132@users.noreply.github.com>
2023-09-20 19:02:25 +0300

    lesson2

--- Mapmanager.py
+++ Mapmanager.py
@@ -3,23 +3,16 @@ import json
 
 class Mapmanager():
     def __init__(self):
-        self.model = 'mymodel/untitled.gltf'  # модель куба у файлі block.egg
-        self.texture = 'block.png'  # текстура куба
+        self.model = 'blocks/grass-block.glb'  # модель куба у файлі block.egg
         self.color = (150/255, 32/255, 10/255, 1)  # rgba
         self.startNew()
 
 
     def addBlock(self,position):
         self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))
         self.block.setPos(position)
-        self.block.setColor(self.color)
         self.block.reparentTo(self.land)
-    def addPanda(self, position, size):
-        self.panda = loader.loadModel('mymodel/untitled.gltf')
-        self.panda.setPos(position)
-        self.panda.setScale(size)
-        self.panda.reparentTo(self.land)
+
 
     def startNew(self):
         self.land = render.attachNewNode("Land")
@@ -31,3 +24,5 @@ class Mapmanager():
             for elem in data:
                 if elem["name"] == "block":
                     self.addBlock(tuple(elem["pos"]))
+
+
--- main.py
+++ main.py
@@ -1,14 +1,114 @@
+from math import *
+
 from direct.showbase.ShowBase import ShowBase
+from direct.showbase.ShowBaseGlobal import globalClock
+from panda3d.core import WindowProperties
+
 import Mapmanager
 import simplepbr
+
+def degToRad(degrees):
+    return degrees * (pi / 180.0)
+
+
 class Game(ShowBase):
-  def __init__(self):
-      super().__init__(self)
+    def __init__(self):
+        super().__init__(self)
 
-      simplepbr.init()
-      land = Mapmanager.Mapmanager()
-      land.loadLand()
+        land = Mapmanager.Mapmanager()
+        land.loadLand()
+        self.captureMouse()
+        self.setupCamera()
+        self.setupControls()
 
+        self.taskMgr.add(self.update, 'update')
+    def setupControls(self):
+        self.keyMap = {
+            "forward": False,
+            "backward": False,
+            "left": False,
+            "right": False,
+            "up": False,
+            "down": False,
+        }
+        self.accept('w', self.updateKeyMap, ['forward', True])
+        self.accept('w-up', self.updateKeyMap, ['forward', False])
+        self.accept('a', self.updateKeyMap, ['left', True])
+        self.accept('a-up', self.updateKeyMap, ['left', False])
+        self.accept('s', self.updateKeyMap, ['backward', True])
+        self.accept('s-up', self.updateKeyMap, ['backward', False])
+        self.accept('d', self.updateKeyMap, ['right', True])
+        self.accept('d-up', self.updateKeyMap, ['right', False])
+        self.accept('space', self.updateKeyMap, ['up', True])
+        self.accept('space-up', self.updateKeyMap, ['up', False])
+        self.accept('lshift', self.updateKeyMap, ['down', True])
+        self.accept('lshift-up', self.updateKeyMap, ['down', False])
+
+    def updateKeyMap(self, key, value):
+        self.keyMap[key] = value
+
+    def setupCamera(self):
+        self.disableMouse()
+        self.camera.setPos(0, -3, 3)
+        self.camLens.setFov(80)
+
+    def update(self, task):
+        dt = globalClock.getDt()
+
+        playerMoveSpeed = 10
+
+        x_movement = 0
+        y_movement = 0
+        z_movement = 0
+        if self.keyMap['forward']:
+            x_movement -= dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+            y_movement += dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+        if self.keyMap['backward']:
+            x_movement += dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+            y_movement -= dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+        if self.keyMap['left']:
+            x_movement -= dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+            y_movement -= dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+        if self.keyMap['right']:
+            x_movement += dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+            y_movement += dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+        if self.keyMap['up']:
+            z_movement += dt * playerMoveSpeed
+        if self.keyMap['down']:
+            z_movement -= dt * playerMoveSpeed
+
+        self.camera.setPos(
+            self.camera.getX() + x_movement,
+            self.camera.getY() + y_movement,
+            self.camera.getZ() + z_movement,
+        )
+
+        md = self.win.getPointer(0)
+        mouseX = md.getX()
+        mouseY = md.getY()
+
+        mouseChangeX = mouseX - self.win.getXSize() // 2
+        mouseChangeY = mouseY - self.win.getYSize() // 2
+
+        self.cameraSwingFactor = 10  # чутливість
+
+        currentH = self.camera.getH()
+        currentP = self.camera.getP()
+
+        self.camera.setHpr(
+            currentH - mouseChangeX * dt * self.cameraSwingFactor,
+            currentP - mouseChangeY * dt * self.cameraSwingFactor,
+            0
+        )
+
+        self.win.movePointer(0, self.win.getXSize() // 2, self.win.getYSize() // 2)
+
+        return task.cont
+    def captureMouse(self):
+        properties = WindowProperties()
+        properties.setCursorHidden(True)
+        properties.setMouseMode(WindowProperties.M_relative)
+        self.win.requestProperties(properties)
 
 
 game = Game()
